eba21e74f216d37a07a08411acc55384965fb93d
Christoffer Holmström <a20chrho@student.his.se>
2021-05-20 23:49:35 +0200

    - added a gui (non-functional)

---
+++ app/src/main/java/com/example/projectproject/GUI_Buttons.java
@@ -0,0 +1,64 @@
+package com.example.projectproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GUI_Buttons#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GUI_Buttons extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public GUI_Buttons() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GUI_Buttons.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GUI_Buttons newInstance(String param1, String param2) {
+        GUI_Buttons fragment = new GUI_Buttons();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_g_u_i__buttons, container, false);
+    }
+}
