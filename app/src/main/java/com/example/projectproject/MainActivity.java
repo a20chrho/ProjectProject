42eab73371e5e588960c03f2d7f1795b3b79d056
Christoffer Holmström <a20chrho@student.his.se>
2021-05-22 09:38:20 +0200

    - fixed the error that prevented access to the second activity

--- app/src/main/java/com/example/projectproject/MainActivity.java
+++ app/src/main/java/com/example/projectproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps Button1 */
     public void GoToAct1(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/projectproject/SecondActivity.java
+++ app/src/main/java/com/example/projectproject/SecondActivity.java
@@ -11,7 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
     /** Called when the user taps the Button1 */
